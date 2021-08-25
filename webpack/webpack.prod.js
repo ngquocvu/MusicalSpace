8746a1327239791f0690dcd8dc339aeaf654ec83
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-25 20:59:37 +0700

    fix bugs

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = {
     new webpack.DefinePlugin({
       process: {
         env: {
-          NODE_ENV: 'production',
+          NODE_ENV: `"production"`,
         },
       },
     }),
