65903fee1d3c71b2006fde0a70e61220a07e16f0
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-25 20:21:03 +0700

    fix my bugs againnnn

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,13 @@
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-const Dotenv = require('dotenv-webpack')
+const dotenv = require('dotenv')
 const webpack = require('webpack')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [new ReactRefreshWebpackPlugin()],
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed),
+    }),
+  ],
 }
