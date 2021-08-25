fcd18a178f1edb096d1c7240df8903f9d211ef2f
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-25 20:15:15 +0700

    Fixing my stupid bugs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,6 @@
-const { REACT_APP_CLIENT_ID, REACT_APP_AUTHORIZE_URL, REACT_APP_REDIRECT_URL } =
-  process.env;
 const handleLogin = () => {
-  window.location.href = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
-};
+  window.location.href = `${process.env.REACT_APP_AUTHORIZE_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`
+}
 const Home = () => {
   return (
     <div className="container p-6 mx-auto flex justify-center text-center">
@@ -13,7 +11,7 @@ const Home = () => {
         Login to spotify
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,12 +1,8 @@
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 const webpack = require('webpack')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [
-    new ReactRefreshWebpackPlugin(),
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-  ],
+  plugins: [new ReactRefreshWebpackPlugin()],
 }
