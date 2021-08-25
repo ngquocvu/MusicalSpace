53f9880f10778535f1862664513bee4ef6cc685b
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-25 19:22:13 +0700

    Fix Webpack Production Build ENV Bug

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,11 +1,6 @@
 const webpack = require('webpack')
-const dotenv = require('dotenv')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(dotenv.config().parsed), // it will automatically pick up key values from .env file
-    }),
-  ],
+  plugins: [new ReactRefreshWebpackPlugin()],
 }
