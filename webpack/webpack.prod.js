0b67aaec9069ae61da6d80a76c9a03860f2ed214
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-26 12:05:42 +0700

    Set systemvars: true

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,7 @@
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
-const webpack = require('webpack')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [new ReactRefreshWebpackPlugin(), new Dotenv()],
+  plugins: [new ReactRefreshWebpackPlugin(), new Dotenv({ systemvars: true })],
 }
