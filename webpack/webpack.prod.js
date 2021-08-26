e25c463fe7ebe97c0677c5283e3c868807235f43
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-26 11:44:45 +0700

    Fix my f*cking problem

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,17 +1,8 @@
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 const webpack = require('webpack')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [
-    new ReactRefreshWebpackPlugin(),
-    new webpack.DefinePlugin({
-      process: {
-        env: {
-          NODE_ENV: `"production"`,
-        },
-      },
-    }),
-  ],
+  plugins: [new ReactRefreshWebpackPlugin(), new Dotenv()],
 }
